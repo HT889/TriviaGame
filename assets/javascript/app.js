94d874e7b47695801ad2428c412b20a3b5d92a19
Haig Talarian <haigtalarian@gmail.com>
2019-06-22 15:45:19 -0700

    updated javascript with Hanna's help

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,7 @@ var questions = [
 var rightAnswers = 0;
 // var wrongAnswers = 0;
 var timer;
-var counter = 30;
+var counter = 20;
 var rollSound2 = new Audio("./assets/audiofiles/Corvette+pass.mp3");
 var yay = new Audio("./assets/audiofiles/1_person_cheering-Jett_Rifkin-1851518140.mp3");
 
@@ -37,42 +37,96 @@ $('#start').click(e => rollSound2.play());
 
 
 var addQuestions = function () {  //function to generate questions based on the array above
+    let quizArea = $('#quiz-area');
+
     for (let i = 0; i < questions.length; i++) {
-        $("#quiz-area").append("<h4>" + questions[i].question + "</h4>");
+        let question = $('<div>').attr('id', `question-div-${i}`);
+        question.append("<h4>" + questions[i].question + "</h4>");
         for (let j = 0; j < questions[i].answers.length; j++) {
-            $("#quiz-area").append(`<input class='question' type="radio" name="question-${i}" value="${questions[i].answers[j]}">` + `<span class="answer">` + questions[i].answers[j]) + `</span>`;
+            question.append(`<input class='question' type="radio" name="question-${i}" value="${questions[i].answers[j]}">` + `<span class="answer">` + questions[i].answers[j]) + `</span>`;
 
         }
-
+        quizArea.append(question)
     }
+
+}
+function updateScore(array) {
+
+    // console.log('ARRAY', array);
+
+    for (let i = 0; i < array.length; i++ ) {
+        // console.log(array[i].value, questions[i].correctAnswer);
+        if ( array[i].value === questions[i].correctAnswer ) {
+            rightAnswers++
+        } 
+    }
+    // console.log("Final Score:", rightAnswers);
 }
 
 function checkAnswers() {  // function checks how many wrong and right answers are picked
     yay.play();
-    var answerCheck = $(`#quiz-area`).children(`input:checked`);
+    // var answerCheck = $(`#quiz-area`).children(`input:checked`);
+    let answer = {};
+    let checkAnswers = [];
+  
+    var answerCheck1 = $(`#question-div-0`).children(`input:checked`); 
+    var answerCheck2 = $(`#question-div-1`).children(`input:checked`);
+    var answerCheck3 = $(`#question-div-2`).children(`input:checked`);
+    var answerCheck4 = $(`#question-div-3`).children(`input:checked`);
 
-    console.log(answerCheck);
-    // if ((answerNotChecked.length % 4) == 1) {
-    //     console.log(answerNotChecked);        
-    // }
-    // console.log(answerNotChecked.length % 4);
-
-    // console.log(answerCheck);
-    for (let i = 0; i < answerCheck.length; i++) {
-        var currentValue = answerCheck[i].value;
-        // console.log(currentValue);
-        if (currentValue === questions[i].correctAnswer) {
-            rightAnswers++;
-            }
-        // else if (currentValue != questions[i].correctAnswer) {
-        //     wrongAnswers+=1;
-        // }
-        // else {
-        //     wrongAnswers+=1;
-        // }
- 
+    if ( answerCheck1.length === 1 ) {
+        answer.value = answerCheck1[0].value;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
+    } else {
+        answer.value = 0;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
     }
-    
+
+    if ( answerCheck2.length === 1 ) {
+        answer.value = answerCheck2[0].value;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
+    } else {
+        answer.value = 0;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
+    }
+
+    if ( answerCheck3.length === 1 ) {
+        answer.value = answerCheck3[0].value;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
+    } else {
+        answer.value = 0;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
+    }
+
+    if ( answerCheck4.length === 1 ) {
+        answer.value = answerCheck4[0].value;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
+    } else {
+        answer.value = 0;
+        checkAnswers.push(answer);
+        answer = {};
+        // console.log(checkAnswers);
+    }
+
+    // console.log('checkAnswers Array', checkAnswers);
+    updateScore(checkAnswers);
+
+    // console.log('AFTER FUNCTIONS RUN', rightAnswers)
+
     alert(`You got ` + rightAnswers + ` out of ` + questions.length + ` questions correct!` + `\n` + `\n` + `Refresh the page if you want to try again.`);
 
 }
@@ -121,3 +175,27 @@ $(document).ready(function () {
 //assigning right/wrong answers
 //timer based game
 //tally up all right answers + wrong answers at the end of timer and display it to player
+
+
+
+    // if ((answerNotChecked.length % 4) == 1) {
+    //     console.log(answerNotChecked);        
+    // }
+    // console.log(answerNotChecked.length % 4);
+
+    // console.log(answerCheck);
+    // for (let i = 0; i < answerCheck.length; i++) {
+    //     var currentValue = answerCheck[i].value;
+    //     console.log(currentValue);
+    //     if (currentValue === questions[i].correctAnswer) {
+    //         rightAnswers++;
+    //         }
+        // else if (currentValue != questions[i].correctAnswer) {
+        //     wrongAnswers+=1;
+        // }
+        // else {
+        //     wrongAnswers+=1;
+        // }
+ 
+    // }
+    
